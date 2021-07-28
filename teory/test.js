ee326cda5bcd20fa12c0900fdbe9debb35ff9849
AVN <avnesterov88@gmail.com>
2021-07-28 16:48:38 +0700

    r 2.17 9м

---
+++ teory/test.js
@@ -0,0 +1,25 @@
+// цыклы
+
+let num = 50;
+
+// цыкл while
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// цыкл do while
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 55);
+
+// цыкл for
+for (let i = 0; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+    //num++;
+}
