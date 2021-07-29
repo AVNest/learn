41f106a17106a48068d83d35ac896d65ee390ee5
AVN <avnesterov88@gmail.com>
2021-07-29 15:26:01 +0700

    r 2.19

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt('сколько фильмов вы посмотрели', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('сколько фильмов вы посмотрели', '');
+
+    while(numberOfFilms == ''|| numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('сколько фильмов вы посмотрели', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,18 +18,24 @@ const personalMovieDB = {
     privat: false
 };
 
-for(let i = 0; i < 2; i++) {
-    const a = prompt('Один из посмотерных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('Один из посмотерных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
+}
 
+// rememberMyFilms();
+
+function detectPesonalLevel() {
     if (personalMovieDB.count < 10) {
         console.log('Просмотрено довольно мало фильмов');
     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -29,8 +45,22 @@ for(let i = 0; i < 2; i++) {
     } else {
         console.log('Произошла ошибка');
     }
-
-    
 }
 
-console.log(personalMovieDB);
+// detectPesonalLevel();
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+} 
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`ваш любимый жанр под номером ${1}`);
+    }
+}
+
+writeYourGenres();
---
+++ teory/test3.js
@@ -0,0 +1,18 @@
+
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf("f"));
+
+const logg = "hello world";
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.23;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseFloat(test));
