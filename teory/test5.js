5e74f6790e1cdba84451b6f4dc5856ba93cb2401
AVN <avnesterov88@gmail.com>
2021-08-03 19:39:53 +0700

    r 2.22

---
+++ teory/test5.js
@@ -0,0 +1,33 @@
+
+const arr = [10, 21, 13, 26, 18];
+arr.sort();
+console.log(arr);
+
+function compareNum(a, b) {
+    return (a - b);
+}
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} в массиве ${arr}`);
+// });
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
