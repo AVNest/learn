392b4e74f73403d92a0e1382cc7cb47f7667c34c
AVN <avnesterov88@gmail.com>
2021-08-07 19:07:36 +0700

    r 2.24

--- teory/test6.js
+++ teory/test6.js
@@ -80,3 +80,25 @@ const video = ['youtube', 'vimeo', 'rutube'],
       internet = [...video, ...blogs, 'vk', 'facebook'];
 
 console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [5, 7, 10];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+console.log(newObj);
