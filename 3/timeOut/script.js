7c8e0c3c6b8eb0fb8f3d3dbdabc2fa5515a55162
AVN <avnesterov88@gmail.com>
2021-10-28 15:27:47 +0700

    r 3.40

--- 3/timeOut/script.js
+++ 3/timeOut/script.js
@@ -6,10 +6,20 @@ function myAnimation() {
     const elem = document.querySelector('.box');
     let pos = 0;
 
-    function frame() {
+    const id = setInterval(frame, 10);
 
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
     }
 }    
+
+btn.addEventListener('click', myAnimation);
 
 // btn.addEventListener('click', () => {
 //     // const timerId = setTimeout(logger, 2000);
