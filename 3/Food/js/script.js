409dd7cfc6b01c25c6720c96864f85ec02e98cee
AVN <avnesterov88@gmail.com>
2021-11-09 16:57:36 +0700

    r3.41 22 00
    Timer dead line

--- 3/Food/js/script.js
+++ 3/Food/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -35,4 +36,49 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // dead Line timer
+
+    const deadLine = '2021-11-12';
+
+    function getTimerRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor((t / (1000 * 60 * 60 * 24))),
+              hours = Math.floor((t / (1000 * 60 *60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+    
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();      
+
+        function updateClock() {
+            const t = getTimerRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadLine);
 });
